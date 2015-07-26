b2f56311fa0c51d000196040225912a35f3eecbe
Ramun McCallum <matthew@synapseware.com>
2015-07-26 14:48:36 -0700

    ADC smoothing removed, timer changed

--- hb-driver.c
+++ hb-driver.c
@@ -1,8 +1,6 @@
 #include "hb-driver.h"
 
 
-volatile int	_adc_result = 0;
-
 //-------------------------------------------------------
 // Timer0 is used for 8-bit PWM output, clk/1, OC0A
 void init_timer0(void)
@@ -14,66 +12,58 @@ void init_timer0(void)
                 (1<<WGM00);		// Fast PWM
 
     // Timer/Counter Control Register B
-    TCCR0B  =   (1<<WGM02)  |	// Fast PWM
+    TCCR0B  =   (0<<WGM02)  |	// Fast PWM
                 (0<<CS02)   |	// clk/1
-                (0<<CS01)   |	// ...
+                (1<<CS01)   |	// ...
                 (1<<CS00);		// ...
 
     // Output Compare Register A
-    OCR0A   =   127;
+    OCR0A   =   0;
 
     // Output Compare Register B
-    OCR0B   =   127;
+    OCR0B   =   0xFF;
 
-    // Timer/Counter Interrupt Mask Register
-    TIMSK0  =   0;
-
-    // Set PWM OC0A as output
+    // Set PWM OC0A as output, start low
     PWM_PORT &= ~(1<<PWM_OUTPUT);
     PWM_DDR	|=	(1<<PWM_OUTPUT);
 }
 
 
 //-------------------------------------------------------
-// The ADC is hardwired to read from ADC7
+// The ADC is hardwired to read from ADC7, free-running, self triggering
 void init_adc(void)
 {
     // ADC Multiplexer Selection Register
-    ADMUX   =   (0<<REFS1)  |   // 5v ref
-                (0<<REFS0)  |   // ...
+    ADMUX   =   (0<<REFS1)  |   // Analog reference
+                (1<<REFS0)  |   // ...
                 (1<<ADLAR)  |   // Left Adjust Result (so we can just read ADCH and be done)
                 (0<<MUX4)   |   // Bits 4:0 – MUX4:0: Analog Channel Selection
-                (0<<MUX3)   |   // ADC7
-                (1<<MUX2)   |   // ADC7
-                (1<<MUX1)   |   // ADC7
-                (1<<MUX0);      // ADC7
+                (0<<MUX3)   |   // ADC0
+                (0<<MUX2)   |   // ADC0
+                (0<<MUX1)   |   // ADC0
+                (0<<MUX0);      // ADC0
 
     // ADC Control and Status Register A
-    ADCSRA  =   (1<<ADEN)   |   // Bit 7 – ADEN: ADC Enable
-                (0<<ADSC)   |   // Bit 6 – ADSC: ADC Start Conversion
-                (0<<ADATE)  |   // Bit 5 – ADATE: ADC Auto Trigger Enable
-                (0<<ADIF)   |   // Bit 4 – ADIF: ADC Interrupt Flag
-                (1<<ADIE)   |   // Bit 3 – ADdIE: ADC Interrupt Enable
+    ADCSRA  =   (1<<ADEN)   |   // ADC Enable
+                (0<<ADSC)   |   // Don't start a conversion
+                (1<<ADATE)  |   // Enable ADC Auto Trigger Enable
+                (1<<ADIE)   |   // ADC Interrupt Enable
                 (1<<ADPS2)  |   // XTAL/128
                 (1<<ADPS1)  |   // XTAL/128
                 (1<<ADPS0);     // XTAL/128
 
     // ADC Control and Status Register B
-    ADCSRB  =   (0<<ADHSM)  |   // Bit 7 – ADHSM: ADC High Speed Mode
-                (0<<ACME)   |   // Bit 6 – ACME: Analog Comparator Mux Enable
-                (0<<MUX5)   |   // ADC7
-                (0<<ADTS3)  |   // T/C1 - Compare Match B
-                (1<<ADTS2)  |   // T/C1 - Compare Match B
-                (0<<ADTS1)  |   // T/C1 - Compare Match B
-                (1<<ADTS0);     // T/C1 - Compare Match B
-    
-    // Digital Input Disable Register 0
-    // Bit 7:4, 1:0 – ADC1:0 Digital Input Disable
-    DIDR0   =   (1<<ADC7D);     // Bit 0
+    ADCSRB  =   (0<<MUX5)   |   // ADC0
+                (0<<ADTS3)  |   // Free-running
+                (0<<ADTS2)  |   // ...
+                (0<<ADTS1)  |   // ...
+                (0<<ADTS0);     // ...
+
+    // Disable the digital input on the ADC input line
+    DIDR0 |= (1<<ADC_ON);
 
     // Set the ADC pin as input
-    ADC_DDR &= ~(1<<ADC_INPUT);
-}
+    ADC_DDR &= ~(1<<ADC_INPUT);}
 
 
 //-------------------------------------------------------
@@ -82,6 +72,16 @@ void setup(void)
 {
 	init_timer0();
 	init_adc();
+
+	power_adc_enable();
+	power_timer0_enable();
+
+	// start the first conversion!
+	ADCSRA |= (1<<ADSC);
+
+	// turn on the on-board LED
+	LED_DDR |= (1<<LED_PIN);
+	LED_PORT &= ~(1<<LED_PIN);
 
 	sei();
 }
@@ -95,7 +95,7 @@ int main(void)
 
 	while(1)
 	{
-
+		//no-op
 	}
 
 	return 0;
@@ -104,19 +104,32 @@ int main(void)
 
 //-------------------------------------------------------
 // ADC conversion complete.  ADLAR should be set
+//volatile int data[4];
 ISR(ADC_vect)
 {
-	static uint8_t data[4];
+	uint8_t adc = ADCH;
 
 	// save the diminishing average
-	data[3] = data[2] >> 2;
-	data[2] = data[1] >> 2;
-	data[1] = data[0] >> 2;
-	data[0] = ADCH;
+	/*
+	data[3] = data[2] >> 1;
+	data[2] = data[1] >> 1;
+	data[1] = data[0] >> 1;
+	data[0] = adc;
 
 	// take the average of the data
-	int avg = (data[0] + data[1] + data[2] + data[3]) >> 4;
+	int avg = (data[0] + data[1] + data[2] + data[3]) / 4;
+	*/
 
-	// set the current PWM value to the new average
-	OCR0A = avg & 0xFF;
+	// set the PWM value to the new average
+	OCR0A = adc & 0xFF;
+
+	// diag
+	if (OCR0A > 128)
+	{
+		LED_PORT |= (1<<LED_PIN);
+	}
+	else
+	{
+		LED_PORT &= ~(1<<LED_PIN);
+	}
 }
--- hb-driver.h
+++ hb-driver.h
@@ -8,13 +8,20 @@
 
 
 // ADC input on PF7 (Port A0 on Arduino board)
-#define	ADC_INPUT			PF7
+#define	ADC_INPUT			PF0
 #define ADC_PORT			PORTF
 #define ADC_DDR				DDRF
+#define ADC_ON				ADC0D
 
 // PWM output on PB7 (Port D11 - PWM8/16 on Arduino board)
 #define PWM_OUTPUT			PB7
 #define PWM_PORT			PORTB
 #define PWM_DDR				DDRB
+
+// onboard LED
+#define LED_PIN				PC7
+#define LED_PORT			PORTC
+#define LED_DDR				DDRC
+
 
 # endif
